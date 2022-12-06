8e1651756670531479ed71b45775b4c7df59e667
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-12-06 23:01:05 +0200

    Fixed search suggestion path

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -32,7 +32,7 @@
 
         public async Task<List<string>> GetProductSearchSuggestions(string searchText)
         {
-            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestion/{searchText}");
             return result.Data;
         }
 
