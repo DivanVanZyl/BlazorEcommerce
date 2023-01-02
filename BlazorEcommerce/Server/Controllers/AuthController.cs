4e63dddd866ac36a162564d066ca9d67b16df91d
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-01-02 16:01:47 +0200

    Implement Changing password

--- BlazorEcommerce/Server/Controllers/AuthController.cs
+++ BlazorEcommerce/Server/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BlazorEcommerce.Server.Controllers
 {
@@ -35,6 +37,20 @@ namespace BlazorEcommerce.Server.Controllers
             {
                 return BadRequest(response);
             }
+
+            return Ok(response);
+        }
+
+        [HttpPost("change-password"), Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword([FromBody] string newPassword)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var response = await _authService.ChangePassword(int.Parse(userId), newPassword);
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+
             return Ok(response);
         }
     }
