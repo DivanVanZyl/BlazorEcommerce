164394a914a291a8d8a16e3a6b1ebdd19610a1e8
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-01-22 13:17:33 +0200

    Order service

--- BlazorEcommerce/Server/GlobalUsings.cs
+++ BlazorEcommerce/Server/GlobalUsings.cs
@@ -5,3 +5,4 @@ global using BlazorEcommerce.Server.Services.ProductService;
 global using BlazorEcommerce.Server.Services.CategoryService;
 global using BlazorEcommerce.Server.Services.AuthService;
 global using BlazorEcommerce.Server.Services.CartService;
+global using BlazorEcommerce.Server.Services.OrderService;
--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddJwtBearer(options =>
         {
