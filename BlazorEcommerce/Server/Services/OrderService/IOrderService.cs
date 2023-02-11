e51d6e10522efab530d7b4a48438be1a3b0caa7f
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-02-11 14:48:42 +0200

    Get orders from the server

--- BlazorEcommerce/Server/Services/OrderService/IOrderService.cs
+++ BlazorEcommerce/Server/Services/OrderService/IOrderService.cs
@@ -1,7 +1,10 @@
-﻿namespace BlazorEcommerce.Server.Services.OrderService
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Server.Services.OrderService
 {
     public interface IOrderService
     {
         Task<ServiceResponse<bool>> PlaceOrder();
+        Task<ServiceResponse<List<OrderOverviewResponse>>> GetOrders();
     }
 }
--- BlazorEcommerce/Server/Services/OrderService/OrderService.cs
+++ BlazorEcommerce/Server/Services/OrderService/OrderService.cs
@@ -16,6 +16,31 @@ namespace BlazorEcommerce.Server.Services.OrderService
             _authService = authService;
         }
 
+        public async Task<ServiceResponse<List<OrderOverviewResponse>>> GetOrders()
+        {
+            var response = new ServiceResponse<List<OrderOverviewResponse>>();
+            var orders = await _context.Orders
+                .Include(o => o.OrderItems)
+                .ThenInclude(io => io.Product)
+                .OrderByDescending(o => o.OrderDate)
+                .ToListAsync();
+
+            var orderResponse = new List<OrderOverviewResponse>();
+            orders.ForEach(o => orderResponse.Add(new OrderOverviewResponse
+            {
+                Id = o.Id,
+                OrderDate = o.OrderDate,
+                TotalPrice = o.TotalPrice,
+                Product = o.OrderItems.Count > 1 ?
+                $"{o.OrderItems.First().Product.Title} and " +
+                $" {o.OrderItems.Count - 1} more..." :
+                o.OrderItems.First().Product.Title,
+                ProductImageUrl = o.OrderItems.First().Product.ImageUrl
+            }));
+
+            return response;
+        }
+
         public async Task<ServiceResponse<bool>> PlaceOrder()
         {
             var products = (await _cartService.GetDbCartProducts()).Data;
